96d970d2338c3d0ca05ed886a576f3f47d1d4ecf
AtefehGhanizadeh <94842580+AtefehGhanizadeh@users.noreply.github.com>
2023-11-26 13:38:54 +0330

    fix transaction type color,add redirect

--- src/react-query/hooks/useChangePass.js
+++ src/react-query/hooks/useChangePass.js
@@ -1,14 +1,22 @@
 import { useMutation } from "@tanstack/react-query";
 import APIClientToken from "../services/apiClient-token";
 import { API_ENDPOINTS } from "@/utils/api/endpoints";
+import useShowToast from "@/components/ui/useShowToast";
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
 
 const apiClient = new APIClientToken(API_ENDPOINTS.CHANGE_PASS);
 
 const useChangePass = () => {
+	const showToast = useShowToast();
+	const token = Cookies.get("token");
+	const{push}=useRouter()
 	return useMutation({
 		mutationFn: apiClient.post,
 		onSuccess: (data) => {
-			console.log(data.data);
+			token ? Cookies.remove("token") : "";
+			push("/login");
+			showToast("کاربر عزیز لطفا دوباره وارد شوید.")
 		},
 	});
 };
