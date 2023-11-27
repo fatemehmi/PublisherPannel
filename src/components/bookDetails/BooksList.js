818b11672a11bb7e35ed5014588845775a885eed
AtefehGhanizadeh <94842580+AtefehGhanizadeh@users.noreply.github.com>
2023-11-27 21:25:47 +0330

    adding book detail

---
+++ src/components/bookDetails/BooksList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CustomCardContainer from '../ui/bookDetail/CustomCardContainer'
+
+function BooksList() {
+  return (
+    <CustomCardContainer height="315px" pt="30px">
+        
+    </CustomCardContainer>
+  )
+}
+
+export default BooksList
--- src/components/ui/userProfile/BookCard.js
+++ src/components/ui/userProfile/BookCard.js
@@ -7,6 +7,7 @@ import {
   Box,
   HStack,
   VStack,
+  Center,
 } from "@chakra-ui/react";
 import Link from "next/link";
 
@@ -39,7 +40,7 @@ function BookCard(props) {
               />
             </Link>
             <Stack spacing="3" rowGap="20px" flexGrow="1">
-              <Heading fontWeight="extrabold" fontSize="28px">
+              <Heading fontFamily="Vazirmatn" fontWeight="extrabold" fontSize="28px">
                 {props.bookname}
               </Heading>
               <HStack alignItems="center">
@@ -61,13 +62,16 @@ function BookCard(props) {
               </Stack>
             </Stack>
             <VStack rowGap="8px">
-              <Box
+              <Center
                 width="48px"
                 height="48px"
                 borderRadius="10px"
                 border="1px"
                 borderColor="#C8C8C8"
-              ></Box>
+              ><svg xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24" strokeWidth={1.5} stroke="red" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+            </svg>
+            </Center>
               <span className="text-[12px] font-medium text-[#515457]">
                 لایک
               </span>
